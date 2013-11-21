b3f797f672a91386cad9c8d52a0791d38086e3cf
Tim Lytle <tim@timlytle.net>
2013-11-21 00:16:03 -0500

    Removed 'from' in configuration.

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,6 @@
  */
 defined('NEXMO_KEY') || (getenv('NEXMO_KEY') AND define('NEXMO_KEY', getenv('NEXMO_KEY')));
 defined('NEXMO_SECRET') || (getenv('NEXMO_SECRET') AND define('NEXMO_SECRET', getenv('NEXMO_SECRET')));
-defined('NEXMO_FROM') || (getenv('NEXMO_FROM') AND define('NEXMO_FROM', getenv('NEXMO_FROM')));
 defined('MONGO') || (getenv('MONGO') AND define('MONGO', getenv('MONGO')));
 
 require_once __DIR__ . '/src/Proxy.php';
