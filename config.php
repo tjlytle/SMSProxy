0bbd62c46b07717d1b143a4c8a5ae0b921e2f81a
Tim Lytle <tim@timlytle.net>
2016-06-07 00:04:37 -0400

    Fix for incorrect constant.

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ if(file_exists(__DIR__ . '/.local.php')){
 // try environment
 defined('NEXMO_KEY')    || (getenv('NEXMO_KEY')    AND define('NEXMO_KEY', getenv('NEXMO_KEY')));
 defined('NEXMO_SECRET') || (getenv('NEXMO_SECRET') AND define('NEXMO_SECRET', getenv('NEXMO_SECRET')));
-defined('NEXMO_FROM')   || (getenv('NEXMO_FROM')   AND define('NEXMO_FROM', getenv('NEXMO_FROM')));
+defined('MONGO')        || (getenv('MONGO')        AND define('MONGO', getenv('MONGO')));
 
 // use pirate speak?
 defined('PIRATE') || define('PIRATE', '11/19' == date('m/d'));
