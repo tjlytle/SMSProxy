1ed49beea24923b3d78f0f2450def9ad5e14ff3f
Tim Lytle <tim@timlytle.net>
2016-06-07 00:00:15 -0400

    Forgot to remove include. TODO: use autoloading

--- demo/bootstrap.php
+++ demo/bootstrap.php
@@ -3,4 +3,3 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/src/Proxy.php';
 require_once __DIR__ . '/src/Pirate.php';
-require_once __DIR__ . '/src/Nexmo.php';
